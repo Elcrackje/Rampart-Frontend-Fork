71355a37180fe38e981d5ad20e15dcc5816011e9
estefanojaque <u202225466@upc.edu.pe>
2024-10-31 09:45:04 -0500

    feat:Add page-not-found component

---
+++ src/public/pages/page-not-found.component.vue
@@ -0,0 +1,31 @@
+<script>
+
+export default {
+  name: "page-not-found",
+  computed: {
+    notFoundPath() {
+      return this.$route.path;
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="centered">
+    <h1>404 {{ $t('page-not-found') }}</h1>
+    <p>{{$t('route-not-found')}} {{ notFoundPath }}</p>
+    <router-link to="home">
+      <pv-button label="Ir a Home" severity="danger"></pv-button>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+.centered {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: 2rem;
+}
+</style>
