6240f8bf087906beb828f08d266bdcc9a7804117
estefanojaque <u202225466@upc.edu.pe>
2024-10-31 09:13:52 -0500

    fix:Remove files for future frontend update

--- src/chefs/model/review.entity.js
+++
@@ -1,8 +0,0 @@
-export class ReviewEntity {
-    constructor({id = 0, userId = '', rate = 0.0, content = ""}) {
-        this.id = id;
-        this.userId = userId;
-        this.rate = rate;
-        this.content = content;
-    }
-}
